dd41af9ea67259b0cacacbc032be25294c8ec0f2
Rob Snipe <rob.snipe@pointone.co.uk>
2022-10-01 09:35:24 +0100

    updated rrs.data and removed dbus

--- src/Rrs.Data.SqlServer/SqlServerDelegatorFactory.cs
+++ src/Rrs.Data.SqlServer/SqlServerDelegatorFactory.cs
@@ -2,16 +2,16 @@
 {
     public static class SqlServerDelegatorFactory
     {
-        public static DbDelegator Create(string connectionString, IDelegatorBus dataBus = null)
+        public static DbDelegator Create(string connectionString)
         {
             var f = new SqlServerConnectionFactory(connectionString);
-            return new DbDelegator(f, dataBus);
+            return new DbDelegator(f);
         }
 
-        public static DbDelegator Create(IConnectionProperties connectionProperties, IDelegatorBus dataBus = null)
+        public static DbDelegator Create(IConnectionProperties connectionProperties)
         {
             var f = new SqlServerConnectionFactory(connectionProperties);
-            return new DbDelegator(f, dataBus);
+            return new DbDelegator(f);
         }
     }
 }
